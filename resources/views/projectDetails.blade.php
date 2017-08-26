850a6568d380bd42f51066b4614c5fff1739ab37
Diaztanto <diaztanto.h@gmail.com>
2017-08-26 19:03:38 +0700

    Add project detail view.

---
+++ resources/views/projectDetails.blade.php
@@ -0,0 +1,16 @@
+@extends('base')
+
+@section('title')
+  {{$project->nama}}
+@stop
+
+@section('content')
+  <p>ID proyek: {{$project->id}}</p>
+  <p>Nama proyek: {{$project->nama}}</p>
+  <p>Lokasi: {{$project->lokasi}}</p>
+  <p>Deskripsi: {{$project->deskripsi}}</p>
+  <p>Tanggal mulai: {{$project->tanggal_mulai}}</p>
+  
+  <br><br>
+  <p>Daftar barang: (pending) </p>
+@stop
