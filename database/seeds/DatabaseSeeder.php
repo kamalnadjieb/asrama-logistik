8c4c799f6627d1d7cc8c23ebbefd7b8e66e18c40
Jeremia Kavin Raja Parluhutan <13514060@std.stei.itb.ac.id>
2017-09-04 07:22:23 +0700

    add seeder for tipe pengubahan

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,27 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call(TipePengubahanTableSeeder::class);
+    }
+}
+
+class TipePengubahanTableSeeder extends Seeder {
+
+    public function run()
+    {
+        DB::table('tipe_pengubahan_jumlah_barang')->delete();
+
+        $date = new DateTime;
+        DB::table('tipe_pengubahan_jumlah_barang')->insert([
+            [
+                'nama' => 'Penambahan',
+                'created_at' => $date
+            ],
+            [
+                'nama' => 'Pengurangan',
+                'created_at' => $date
+            ]
+
+        ]);
     }
 }
