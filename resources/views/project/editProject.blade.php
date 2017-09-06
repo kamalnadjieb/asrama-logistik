61ee4030c9a51c155ea7d5e407414047d03c5ab8
Jeremia Kavin Raja Parluhutan <13514060@std.stei.itb.ac.id>
2017-09-07 03:07:34 +0700

    adding the column of jumlah_sebelumnya in history table

--- resources/views/project/editProject.blade.php
+++ resources/views/project/editProject.blade.php
@@ -5,23 +5,28 @@
 @stop
 
 @section('content')
-  <p>ID proyek: {{$project->id}}</p>
-  <p>Nama proyek: {{$project->nama}}</p>
-  <p>Lokasi: {{$project->lokasi}}</p>
-  <p>Deskripsi: {{$project->deskripsi}}</p>
-  <p>Tanggal mulai: {{$project->tanggal_mulai}}</p>
+  <form id="editProject" method="POST" action="{{URL::to('logistik/proyek/tambah/do')}}" onsubmit="return validateStok()">
+    <p>ID proyek: {{$project->id}}</p>
+    <p>Nama proyek: {{$project->nama}}</p>
+    <p>Lokasi: {{$project->lokasi}}</p>
+    <p>Deskripsi: {{$project->deskripsi}}</p>
+    <p>Tanggal mulai: {{$project->tanggal_mulai}}</p>
 
-  <br><br>
-  <p>Daftar barang:</p>
-  @foreach($items as $item)
-    {{$item->nama}} {{$item->stok}}/{{$item->pivot->jumlah}} {{$item->satuan}} <br/>
-    <select>
-      <option value="0">Tidak ada pengubahan</option>
-    @foreach($tipepengubahan as $tipe)
-      <option value="{{$tipe->id}}">{{$tipe->nama}}</option>
+    <br><br>
+    <p>Daftar barang:</p>
+    @foreach($items as $item)
+      {{$item->nama}} {{$item->stok}}/{{$item->pivot->jumlah}} {{$item->satuan}} <br/>
+      <select name="tipe_pengubahan_{{$item->pivot->id}}">
+        <option value="0">Tidak ada pengubahan</option>
+      @foreach($tipepengubahan as $tipe)
+        <option value="{{$tipe->id}}">{{$tipe->nama}}</option>
+      @endforeach
+      </select>
+      <input type="number" name="jumlah_{{$item->pivot->id}}" min="0" max="{{$item->pivot->jumlah}}"> {{$item->satuan}}
     @endforeach
-    </select>
-    <input type="number" name="jumlah" min="0" max="{{$item->pivot->jumlah}}"> {{$item->satuan}}
-  @endforeach
-  <input type="submit" value="submit"/>
+    <input type="submit" value="submit"/>
+  </form>
+@stop
+@section('js')
+  
 @stop
